51a4df870af2758a4ea964669534a3a2d9dcf906
iazadur <iamazadur@gmail.com>
2023-11-11 22:08:19 +0600

    meda data added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from '@/lib/Providers'
+import Meta from '@/components/meta/Meta'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <Providers>
       <html lang="en">
+        <head>
+          <Meta />
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </Providers>
